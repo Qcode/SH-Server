bde984164d5cad05ba16e0324e397c35c2736a49
Ross Evans <therealrossevans@gmail.com>
2018-05-20 22:26:49 -0400

    Recognize game start and assign roles

--- src/GameManager.js
+++ src/GameManager.js
@@ -1,4 +1,6 @@
+import { knuthShuffle } from 'knuth-shuffle';
 import User from './User';
+import playerConfigurations from './PlayerConfigurations';
 
 class GameManager {
   constructor(io) {
@@ -12,14 +14,58 @@ class GameManager {
     this.syncUsers();
   }
 
-  syncUsers() {
-    const dataToSend = {};
+  canStartGame(socketId) {
+    return this.users[socketId].host && Object.keys(this.users).length >= 5;
+  }
+
+  startGame() {
+    this.io.emit('startGame');
+    this.assignRoles();
+    this.syncUsers();
+  }
+
+  assignRoles() {
+    const configuartion = playerConfigurations[Object.keys(this.users).length];
+    const options = [];
+    options.push('hitler');
+    for (let x = 0; x < configuartion.fascists - 1; x += 1) {
+      options.push('fascist');
+    }
+    for (let x = 0; x < configuartion.liberals; x += 1) {
+      options.push('liberal');
+    }
+    knuthShuffle(options);
+    let iterator = 0;
     Object.keys(this.users).forEach((key) => {
-      dataToSend[key] = this.users[key].getInfo();
+      switch (options[iterator]) {
+        case 'hitler': {
+          this.users[key].isLiberal = false;
+          this.users[key].isHitler = true;
+          break;
+        }
+        case 'fascist': {
+          this.users[key].isLiberal = false;
+          this.users[key].isHitler = false;
+          break;
+        }
+        default: {
+          // Liberal
+          this.users[key].isLiberal = true;
+          this.users[key].isHitler = false;
+        }
+      }
+      iterator += 1;
     });
-    Object.keys(this.users).forEach((key) => {
-      dataToSend.primaryUserId = key;
-      this.users[key].socket.emit('users', dataToSend);
+  }
+
+  syncUsers() {
+    Object.keys(this.users).forEach((userReceivingInformation) => {
+      const dataToSend = {};
+      dataToSend.primaryUserId = userReceivingInformation;
+      Object.keys(this.users).forEach((userData) => {
+        dataToSend[userData] = this.users[userData].getInfo(this.users[userReceivingInformation]);
+      });
+      this.users[userReceivingInformation].socket.emit('users', dataToSend);
     });
   }
 }
---
+++ src/PlayerConfigurations.js
@@ -0,0 +1,8 @@
+export default {
+  5: { fascists: 2, liberals: 3 },
+  6: { fascists: 2, liberals: 4 },
+  7: { fascists: 3, liberals: 4 },
+  8: { fascists: 3, liberals: 5 },
+  9: { fascists: 4, liberals: 5 },
+  10: { fascists: 4, liberals: 6 },
+};
